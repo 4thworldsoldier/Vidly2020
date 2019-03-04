d54f6ecb031c1f429ed8196abdcc7ce1e1285196
victor <vicky262670@gmail.com>
2019-03-04 13:28:36 +0100

    Add project files.

---
+++ Vidly2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vidly2020.MvcApplication" Language="C#" %>
---
+++ Vidly2020/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly2020.Startup))]
+namespace Vidly2020
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
