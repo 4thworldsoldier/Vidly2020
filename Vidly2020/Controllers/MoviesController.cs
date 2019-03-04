be1b93df4ab776e6666c7075d159c6ad39019c00
victor <vicky262670@gmail.com>
2019-03-04 15:51:54 +0100

    chapter1

---
+++ Vidly2020/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2020.Models;
+
+namespace Vidly2020.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index( int? pageindex, string sortby)
+        {
+            //if(!pageindex.HasValue)
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+
+            return View();
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "shrek!" };
+            return View(movie);
+        }
+
+    }
+}
---
+++ Vidly2020/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2020.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
