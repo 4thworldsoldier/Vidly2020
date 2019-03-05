50a152f44ae6d90af14c79f44c2b8bdb4608183d
victor <vicky262670@gmail.com>
2019-03-05 14:58:07 +0100

    chapter3

--- Vidly2020/Models/Customer.cs
+++ Vidly2020/Models/Customer.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly2020.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool  IsSubscribedToNewsLetter { get; set; }
         //nav prop from one type to its related types
